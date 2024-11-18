ba60d7f0bd1ccacc48bf59969ec5ae72f2091a58
denniskp <122602355+denniskp@users.noreply.github.com>
2024-11-18 15:25:20 +0100

    Initial commit

---
+++ src/test/java/de/dhbw/tinf22b6/codespark/CodeSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw.tinf22b6.codespark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
