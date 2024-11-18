4ccc538231a953d67d03985484c61651bb0b6547
denniskp <122602355+denniskp@users.noreply.github.com>
2024-11-18 15:31:14 +0100

    test: rename test class

--- src/test/java/de/dhbw/tinf22b6/codespark/ApplicationTests.java
+++ src/test/java/de/dhbw/tinf22b6/codespark/ApplicationTests.java
@@ -3,11 +3,12 @@ package de.dhbw.tinf22b6.codespark;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CodeSparkApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
+class ApplicationTests {
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
-
 }
