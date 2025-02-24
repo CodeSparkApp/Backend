175cd1ef0ea3ce017ab2a421cb30deeb29d04e1c
denniskp <122602355+denniskp@users.noreply.github.com>
2025-02-25 00:22:10 +0100

    refactor: Add default constructor

--- src/main/java/de/dhbw/tinf22b6/codespark/api/exception/AccountAlreadyExistsException.java
+++ src/main/java/de/dhbw/tinf22b6/codespark/api/exception/AccountAlreadyExistsException.java
@@ -1,6 +1,10 @@
 package de.dhbw.tinf22b6.codespark.api.exception;
 
 public class AccountAlreadyExistsException extends Exception {
+	public AccountAlreadyExistsException() {
+		super();
+	}
+
 	public AccountAlreadyExistsException(String message) {
 		super(message);
 	}
