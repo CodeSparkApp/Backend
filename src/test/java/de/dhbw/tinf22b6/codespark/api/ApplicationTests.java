d7c28f010615cb05450c87218653c943d1fe7a95
denniskp <122602355+denniskp@users.noreply.github.com>
2025-03-29 00:14:09 +0100

    fix: Use correct application profile

--- src/test/java/de/dhbw/tinf22b6/codespark/api/ApplicationTests.java
+++ src/test/java/de/dhbw/tinf22b6/codespark/api/ApplicationTests.java
@@ -2,9 +2,11 @@ package de.dhbw.tinf22b6.codespark.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ApplicationTests {
 	@Test
